3f61b416c613b235ce20a46a208d4a4eabddd40d
seanlewis413 <seanlewis413@gmail.com>
2017-11-07 14:04:15 -0500

    Base front end template

---
+++ wellnessapp/platforms/ios/build/wellnessapp.build/Debug-iphonesimulator/wellnessapp.build/Objects-normal/i386/main.d
@@ -0,0 +1,11 @@
+dependencies: \
+  /Users/Sean/Documents/SMC/Projects/WellnessApp/wellnessapp/platforms/ios/wellnessapp/wellnessapp-Prefix.pch \
+  /Users/Sean/Documents/SMC/Projects/WellnessApp/wellnessapp/platforms/ios/internal/main.m \
+  /Users/Sean/Documents/SMC/Projects/WellnessApp/wellnessapp/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Users/Sean/Documents/SMC/Projects/WellnessApp/wellnessapp/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntimeInstrumentation.h \
+  /Users/Sean/Documents/SMC/Projects/WellnessApp/wellnessapp/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Users/Sean/Documents/SMC/Projects/WellnessApp/wellnessapp/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Users/Sean/Documents/SMC/Projects/WellnessApp/wellnessapp/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Users/Sean/Documents/SMC/Projects/WellnessApp/wellnessapp/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/Sean/Documents/SMC/Projects/WellnessApp/wellnessapp/platforms/ios/internal/TNSDebugging.h \
+  /Users/Sean/Documents/SMC/Projects/WellnessApp/wellnessapp/platforms/ios/internal/TKLiveSync/include/TKLiveSync.h
